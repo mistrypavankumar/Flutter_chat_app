57c18125b167cb3d4779a5bb52b30168672261b0
mg143pavankumar <pavansharma.mg143@gmail.com>
2020-08-12 23:18:52 +0530

    Fixed sign_in and sign_up design

--- lib/views/sign_up.dart
+++ lib/views/sign_up.dart
@@ -1,3 +1,4 @@
+import 'package:chat_app/services/auth.dart';
 import 'package:chat_app/widgets/widgets.dart';
 import 'package:flutter/material.dart';
 
@@ -7,8 +8,11 @@ class SignUP extends StatefulWidget {
 }
 
 class _SignUPState extends State<SignUP> {
-  final formKey = GlobalKey<FormState>();
+  //loading....
+  bool isloading = false;
+  AuthMethods authMethods = new AuthMethods();
 
+  final formKey = GlobalKey<FormState>();
   TextEditingController userNameTextEditingController =
       new TextEditingController();
 
@@ -18,144 +22,170 @@ class _SignUPState extends State<SignUP> {
   TextEditingController passwordTextEditingController =
       new TextEditingController();
 
-// // loading option
-// signMeUP() {
-//   if (formKey.currentState.validate()) {}
+  // loading option
+  signMeUP() {
+    if (formKey.currentState.validate()) {
+      setState(() {
+        isloading = true;
+      });
+
+      authMethods
+          .signUpWithEmailAndPassword(emailTextEditingController.text,
+              passwordTextEditingController.text)
+          .then((val) => () {
+                print("$val");
+
+                //Navigation to another screen
+              });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: appBarMain(context),
-      body: Container(
-        alignment: Alignment.center,
-        child: Container(
-          padding: EdgeInsets.symmetric(horizontal: 24.0),
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Form(
-                key: formKey,
-                child: Column(
-                  children: [
-                    TextFormField(
-                      validator: (val) {
-                        return val.isEmpty || val.length < 3
-                            ? "Please provide a valid username"
-                            : null;
-                      },
-                      controller: userNameTextEditingController,
-                      style: simpleTextStyle(),
-                      decoration: textFieldInputDecoration("username"),
-                    ),
-                    TextFormField(
-                      validator: (val) {
-                        return RegExp(
-                                    r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
-                                .hasMatch(val)
-                            ? null
-                            : "please provide a valid email.";
-                      },
-                      controller: emailTextEditingController,
-                      style: simpleTextStyle(),
-                      decoration: textFieldInputDecoration("email"),
-                    ),
-                    TextFormField(
-                      validator: (val) {
-                        return val.length > 6
-                            ? null
-                            : "please provide password 6+ character";
-                      },
-                      controller: passwordTextEditingController,
-                      style: simpleTextStyle(),
-                      decoration: textFieldInputDecoration("password"),
-                    ),
-                  ],
-                ),
-              ),
-              SizedBox(
-                height: 8.0,
-              ),
-              Container(
-                alignment: Alignment.centerRight,
+      body: isloading
+          ? Container(
+              child: Center(child: CircularProgressIndicator()),
+            )
+          : SingleChildScrollView(
+              child: Container(
+                height: MediaQuery.of(context).size.height - 150.0,
+                alignment: Alignment.bottomCenter,
                 child: Container(
-                  padding:
-                      EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
-                  child: Text(
-                    "Forgot Password?",
-                    style: simpleTextStyle(),
+                  padding: EdgeInsets.symmetric(horizontal: 24.0),
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Form(
+                        key: formKey,
+                        child: Column(
+                          children: [
+                            TextFormField(
+                              validator: (val) {
+                                return val.isEmpty || val.length < 3
+                                    ? "Please provide a valid username"
+                                    : null;
+                              },
+                              controller: userNameTextEditingController,
+                              style: simpleTextStyle(),
+                              decoration: textFieldInputDecoration("username"),
+                            ),
+                            TextFormField(
+                              validator: (val) {
+                                return RegExp(
+                                            r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
+                                        .hasMatch(val)
+                                    ? null
+                                    : "please provide a valid email.";
+                              },
+                              controller: emailTextEditingController,
+                              style: simpleTextStyle(),
+                              decoration: textFieldInputDecoration("email"),
+                            ),
+                            TextFormField(
+                              obscureText: true,
+                              validator: (val) {
+                                return val.length > 6
+                                    ? null
+                                    : "please provide password 6+ character";
+                              },
+                              controller: passwordTextEditingController,
+                              style: simpleTextStyle(),
+                              decoration: textFieldInputDecoration("password"),
+                            ),
+                          ],
+                        ),
+                      ),
+                      SizedBox(
+                        height: 8.0,
+                      ),
+                      Container(
+                        alignment: Alignment.centerRight,
+                        child: Container(
+                          padding: EdgeInsets.symmetric(
+                              horizontal: 16.0, vertical: 8.0),
+                          child: Text(
+                            "Forgot Password?",
+                            style: simpleTextStyle(),
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 8.0,
+                      ),
+                      GestureDetector(
+                        onTap: () {
+                          //When we click on sign in button
+                          signMeUP();
+                        },
+                        child: Container(
+                          alignment: Alignment.center,
+                          width: MediaQuery.of(context).size.width,
+                          padding: EdgeInsets.symmetric(vertical: 20.0),
+                          decoration: BoxDecoration(
+                            gradient: LinearGradient(
+                              colors: [
+                                const Color(0xff007EF4),
+                                const Color(0xff2A75BC),
+                              ],
+                            ),
+                            borderRadius: BorderRadius.circular(30.0),
+                          ),
+                          child: Text(
+                            "Sign Up",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 17,
+                            ),
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 16.0),
+                      Container(
+                        alignment: Alignment.center,
+                        width: MediaQuery.of(context).size.width,
+                        padding: EdgeInsets.symmetric(vertical: 20.0),
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(30.0),
+                        ),
+                        child: Text(
+                          "Sign Up with Google",
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 17,
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 16.0,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            "Already have an account? ",
+                            style: mediumTextStyle(),
+                          ),
+                          Text(
+                            "Sign in now",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 17.0,
+                              decoration: TextDecoration.underline,
+                            ),
+                          ),
+                        ],
+                      ),
+                      SizedBox(
+                        height: 150.0,
+                      )
+                    ],
                   ),
                 ),
               ),
-              SizedBox(
-                height: 8.0,
-              ),
-              GestureDetector(
-                onTap: () {
-                  //When we click on sign in button
-                },
-                child: Container(
-                  alignment: Alignment.center,
-                  width: MediaQuery.of(context).size.width,
-                  padding: EdgeInsets.symmetric(vertical: 20.0),
-                  decoration: BoxDecoration(
-                    gradient: LinearGradient(
-                      colors: [
-                        const Color(0xff007EF4),
-                        const Color(0xff2A75BC),
-                      ],
-                    ),
-                    borderRadius: BorderRadius.circular(30.0),
-                  ),
-                  child: Text(
-                    "Sign Up",
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 17,
-                    ),
-                  ),
-                ),
-              ),
-              SizedBox(height: 16.0),
-              Container(
-                alignment: Alignment.center,
-                width: MediaQuery.of(context).size.width,
-                padding: EdgeInsets.symmetric(vertical: 20.0),
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(30.0),
-                ),
-                child: Text(
-                  "Sign Up with Google",
-                  style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 17,
-                  ),
-                ),
-              ),
-              SizedBox(
-                height: 16.0,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    "Already have an account? ",
-                    style: mediumTextStyle(),
-                  ),
-                  Text(
-                    "Sign in now",
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 17.0,
-                      decoration: TextDecoration.underline,
-                    ),
-                  ),
-                ],
-              ),
-            ],
-          ),
-        ),
-      ),
+            ),
     );
   }
 }
